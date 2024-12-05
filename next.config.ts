4ec2d8b00f449b7393aa67a1035221fd710c8e9f
vctrubio <vctrubio@gmail.com>
2024-12-05 02:56:19 +0100

    config init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
 };
 
 export default nextConfig;
